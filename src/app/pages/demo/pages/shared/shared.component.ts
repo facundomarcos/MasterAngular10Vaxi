a5f01a10c7122dd5f3a1107d7ee0a258d484f3ec
Facundo <facundomarcos@live.com.ar>
2021-07-16 20:59:13 -0300

    regex en angular

--- src/app/pages/demo/pages/shared/shared.component.ts
+++ src/app/pages/demo/pages/shared/shared.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { regex, regexErrors } from '@app/shared/utils';
 
 @Component({
   selector: 'app-shared',
@@ -10,6 +11,7 @@ export class SharedComponent implements OnInit {
 
   form!: FormGroup;
   isInline!: boolean;
+  regexErrors = regexErrors;
 
   constructor(private fb: FormBuilder) { }
 
@@ -19,7 +21,8 @@ export class SharedComponent implements OnInit {
         updateOn: 'blur',
         validators: [
           Validators.required,
-          Validators.minLength(3)
+          Validators.minLength(3),
+          Validators.pattern(regex.number)
         ]
       }]
     });
