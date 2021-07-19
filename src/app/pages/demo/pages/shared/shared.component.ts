7b00cf64c739090f002680c364ac6f9e2c305310
Facundo <facundomarcos@live.com.ar>
2021-07-19 20:35:00 -0300

    control spinner angular

--- src/app/pages/demo/pages/shared/shared.component.ts
+++ src/app/pages/demo/pages/shared/shared.component.ts
@@ -17,6 +17,8 @@ export class SharedComponent implements OnInit {
 
   items!: ControlItem[];
 
+  showSpinner = false;
+
   constructor(private fb: FormBuilder) {
     this.isInline = true;
     this.items = [
@@ -113,5 +115,17 @@ export class SharedComponent implements OnInit {
      }else{
        this.form.enable();
      }
+  }
+
+  onToggleSpinner(): void{
+    this.showSpinner = !this.showSpinner;
+  }
+
+  onSuccess(): void{
+
+  }
+
+  onError(): void{
+
   }
 }
