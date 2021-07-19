f0ab625a8d87c021a30a54ec630ebf7c0f5a27f3
Facundo <facundomarcos@live.com.ar>
2021-07-19 19:52:20 -0300

    boton grupal y submit validation

--- src/app/pages/demo/pages/shared/shared.component.ts
+++ src/app/pages/demo/pages/shared/shared.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { regex, regexErrors } from '@app/shared/utils';
+import { regex, regexErrors, markFormGroupTouched } from '@app/shared/utils';
 
 import { ControlItem } from '@app/models/frontend';
 
@@ -77,14 +77,41 @@ export class SharedComponent implements OnInit {
   }
 
   onPatchValue(): void {
-    this.form.patchValue({input: 'facundo marcos'});
+    this.form.patchValue(
+      {
+        input: '123',
+        password: 'FacundoMarcos2022',
+        autocomplete: 1,
+        select: 2,
+        checkboxes: [3],
+        radios: 4,
+        date: new Date().getTime(),
+        dateRange: {
+          from: new Date(2021,8,7).getTime(),
+          to: new Date(2021,11,20).getTime(),
+        }
+
+
+    });
   }
 
   onSubmit(): void {
     console.log('Presiono boton submit');
+
+    if(!this.form.valid){
+      markFormGroupTouched(this.form);
+    }
   }
 
   organizarElemento() {
     this.isInline = !this.isInline;
+  }
+
+  onToggleDisabled(): void {
+     if(this.form.enabled){
+       this.form.disable();
+     }else{
+       this.form.enable();
+     }
   }
 }
