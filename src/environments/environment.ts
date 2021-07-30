23e5ce381c2ea21798f76dca3160ff3e5d7a687b
Facundo <facundomarcos@live.com.ar>
2021-07-29 23:58:59 -0300

    effects alta usuario

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -15,6 +15,10 @@ export const environment = {
       appId: "1:162661026563:web:515fe55a8d1c65822c3736"
     }
 
+  },
+  actionCodeSettings: {
+    url: 'http://localhost:5200/demo',
+    handleCodeInApp: true
   }
 };
 
