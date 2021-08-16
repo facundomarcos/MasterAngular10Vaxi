3cf39b6281c307469acf6835009c26bc7f59c409
Facundo <facundomarcos@live.com.ar>
2021-08-16 19:56:29 -0300

    salir sesion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 //import { AngularFirestore } from '@angular/fire/firestore';
-import { Store } from '@ngrx/store';
+import { Store, select } from '@ngrx/store';
 import * as fromRoot from './store';
 import * as fromDictionaries from './store/dictionaries';
-import * as fromUsers from './store/user';
+import * as fromUser from './store/user';
+import { Observable } from 'rxjs';
+
 
 @Component({
   selector: 'app-root',
@@ -12,18 +14,25 @@ import * as fromUsers from './store/user';
 })
 export class AppComponent {
   title = 'ecommerce-angular-app';
+  isAuthorized$ !: Observable<boolean>;
 
  // constructor(private fs: AngularFirestore){}
  //este objeto tiene acceso a toda la data del store
   constructor(private store: Store<fromRoot.State>){}
 
   ngOnInit(){
-    this.store.dispatch(new fromUsers.Init());
+    this.isAuthorized$ = this.store.pipe(select(fromUser.getIsAuthorized))
+
+    this.store.dispatch(new fromUser.Init());
     // this.fs.collection('test').snapshotChanges().subscribe( personas => {
     //   console.log(personas.map( x => x.payload.doc.data()));
     // })
     //dispara el evento para leer firebase y la almacene en el store
     this.store.dispatch(new fromDictionaries.Read());
+  }
+
+  onSignOut() : void {
+    this.store.dispatch(new fromUser.SignOut());
   }
 
 
